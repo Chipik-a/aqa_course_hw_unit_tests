a03244d46b9c4dcab59a2204027f1a948d44c697
Anna Popelniukh <13nuta@gmail.com>
2025-07-30 13:52:07 +0300

    added decision in hw-4 task 3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,23 @@
   преобразовываясь в number
 
 */
+'use strict'
+
+  const minAge = 18;
+  const maxAge = 60;
+  let age = "10";
+
+  if (typeof age === "string" && /^\d+$/.test(age)) {
+      age = Number(age);
+      console.log(typeof(age));
+    }
+  
+  if (age < minAge) {
+    console.log("You don't have access cause your are is " + age + " It's less then");
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!")
+  } else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel")
+  } else {
+    console.log("Technical work")
+  }
